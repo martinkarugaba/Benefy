f0654162c10f7bb9deb7741428275de2014b15b8
Martin Karugaba <martinkarugaba21@gmail.com>
2024-08-05 00:41:16 +0300

    The form submits values successfully

--- components/RegisterParticipant.tsx
+++ components/RegisterParticipant.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
 import { Input } from './ui/input';
+import { Button } from './ui/button';
 
 const RegisterParticipant = () => {
   const {
@@ -11,15 +12,26 @@ const RegisterParticipant = () => {
     reset,
     getValues,
   } = useForm();
+
+  const onSubmit = async (data: FieldValues) => {
+    // submit data to server
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    reset();
+  };
+
   return (
-    <form className="flex flex-col w-full justify-center items-center gap-6">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col w-full justify-center items-center gap-6"
+    >
       <Input
         {...register('email', { required: 'Email is required' })}
         type="email"
-        name=""
-        id=""
         placeholder="Email"
       />
+      {errors.email && (
+        <p className="text-destructive">{`${errors.email.message}`}</p>
+      )}
       <Input
         {...register('password', {
           required: 'Password is required',
@@ -29,19 +41,26 @@ const RegisterParticipant = () => {
           },
         })}
         type="password"
-        name=""
-        id=""
         placeholder="Password"
       />
+      {errors.password && (
+        <p className="text-destructive">{`${errors.password.message}`}</p>
+      )}
       <Input
         {...register('confirmPassword', {
           required: 'Confirm password is required',
+          validate: (value) =>
+            value === getValues('password') || "Passwords don't match",
         })}
         type="password"
-        name=""
-        id=""
         placeholder="Confirm Password"
       />
+      {errors.confirmPassword && (
+        <p className="text-destructive">{`${errors.confirmPassword.message}`}</p>
+      )}
+      <Button disabled={isSubmitting} type="submit">
+        Submit
+      </Button>
     </form>
   );
 };
